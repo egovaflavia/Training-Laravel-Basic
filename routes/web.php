a791889b92a66b4a6775f9e601a2b24267962fa1
Egova Riva Gustino <50259464+egovaflavia@users.noreply.github.com>
2020-12-30 16:32:43 +0700

    Ok

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('halo', function () {
+    return "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ex repudiandae at placeat, molestiae cum amet explicabo dicta, magnam culpa quidem! Dolorem quisquam labore iste, expedita molestiae quibusdam quas esse.";
+});
+
+Route::get('blog', function () {
+    return view('blog');
+});
